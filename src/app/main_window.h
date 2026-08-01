4070cdd270e18e4373ab7956d9fc6646a1bdc475
ktheagent <110701561+ktheagent@users.noreply.github.com>
2026-08-01 02:35:56 +0000

    Define compact Write workspace shell [skip ci]

--- src/app/main_window.h
+++ src/app/main_window.h
@@ -1,13 +1,50 @@
 #pragma once
 
+#include "core/music_types.h"
+
 #include <QMainWindow>
 
+class QAction;
+class QActionGroup;
+class QDockWidget;
+class QToolBar;
+
 namespace airmon {
+
 class ScoreView;
+
 class MainWindow final : public QMainWindow {
  public:
   MainWindow();
+  ~MainWindow() override = default;
+
+  ScoreView* scoreView() const noexcept { return scoreView_; }
+  QAction* command(const QString& id) const;
+  QToolBar* writeWorkspace() const noexcept { return writeWorkspace_; }
+  QDockWidget* notationKeypad() const noexcept { return notationKeypad_; }
+
+ protected:
+  void closeEvent(QCloseEvent* event) override;
+
  private:
+  QAction* addDurationCommand(QActionGroup* group,
+                              Duration duration,
+                              const QString& name,
+                              const QKeySequence& shortcut);
+  QAction* addVoiceCommand(QActionGroup* group,
+                           Voice voice,
+                           const QString& name,
+                           const QKeySequence& shortcut);
+  QWidget* createGroup(const QString& groupName,
+                       const QList<QAction*>& actions,
+                       const QString& surfaceName);
+  void restoreWorkspaceState();
+  void saveWorkspaceState();
+
   ScoreView* scoreView_{};
+  QToolBar* writeWorkspace_{};
+  QDockWidget* notationKeypad_{};
+  QFont musicFont_;
 };
-}
+
+}  // namespace airmon
